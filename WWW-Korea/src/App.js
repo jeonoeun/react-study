0c8d8ac564cb1cc93c4d2a90cf7caf8d65b5c9f8
Jeongeun Lee <zaekoch@gmail.com>
2025-01-25 00:52:26 +0900

    chore: 임시 주석

--- WWW-Korea/src/App.js
+++ WWW-Korea/src/App.js
@@ -3,13 +3,11 @@ import Wrapper from "./component/Wrapper";
 import Header from "./component/Header";
 import MainVisual from "./component/MainVisual";
 import WWFSponsor from "./component/WWFSponsor";
-import WWFCampaign from "./component/WWFCampaign";
 import { Routes, Route } from "react-router-dom";
 import EarthHour from "./component/EarthHour";
 import Why from "./component/Why";
 import HowTo from "./component/HowTo";
 import Who from "./component/Who";
-import Footer from "./component/Footer";
 
 function App() {
   return (
@@ -25,9 +23,9 @@ function App() {
           <Route path="/howTo" element={<HowTo />} />
           <Route path="*" element={<HowTo />} />
         </Routes>
-        <WWFCampaign />
+        {/* <WWFCampaign /> */}
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </Wrapper>
   );
 }
